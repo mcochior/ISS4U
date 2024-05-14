778606dd4ee4158bbbd8c3b0490b79d87a53424d
Marius Sebastian Cochior <139264593+mcochior@users.noreply.github.com>
2024-05-14 13:45:36 +0200

    all changes

--- api/src/controllers/tasks.ts
+++ api/src/controllers/tasks.ts
@@ -13,6 +13,24 @@ export const getAllTasks = async (req: express.Request, res:express.Response) =>
     }
 }
 
+export const getTask = async (req: express.Request, res:express.Response) => {
+    try{
+        const { nome } = req.params;
+
+        const task = await getTaskByName(nome);
+        
+        if (task) {
+            return res.sendStatus(400);
+        }
+
+        return res.status(200).json(task);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+
 
 
 export const makeTask = async (req: express.Request, res: express.Response) => {
--- api/src/router/tasks.ts
+++ api/src/router/tasks.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import {deleteTask, getAllTasks, makeTask, updateTask} from '../controllers/tasks';
+import {deleteTask, getAllTasks, getTask, makeTask, updateTask} from '../controllers/tasks';
 import { isAuthenticated, isOwner } from '../middlewares';
 
 export default (router: express.Router) => {
     router.get('/tasks', isAuthenticated, getAllTasks);
+    router.get('/tasks/:nome', isAuthenticated, getTask);
     router.put('/tasks', isAuthenticated, makeTask);
     router.delete('/tasks/:nome', isAuthenticated, deleteTask);
     router.patch('/tasks/:nome', isAuthenticated, updateTask);
