bb30e2895d4081d46b10da5aa9ef1ff291185442
mcochior <sebastianmarius.cochior@gmail.com>
2024-05-26 17:36:34 +0200

    Now you can get user by session token and all that, that's pretty cool.

--- api/src/router/users.ts
+++ api/src/router/users.ts
@@ -7,6 +7,6 @@ export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
     router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
     router.patch('/users/:id', isAuthenticated, isOwner, updateUser);
-    router.post('/users/:session_token', getUserBySesToken)
+    router.post('/users/:session_token', getUserBySesToken);
 };
 
