4712ece3c73b1944de6ddc42f4456a1a39aa0507
mcochior <sebastianmarius.cochior@gmail.com>
2024-06-11 15:39:05 +0200

    TESTING WORKS!

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,5 +1,14 @@
-export const preset = 'ts-jest';
-export const transform = {
-  '^.+\\.(ts|tsx)?$': 'ts-jest',
-  '^.+\\.(js|jsx)$': 'babel-jest',
+// export const preset = 'ts-jest';
+// export const transform = {
+//   '^.+\\.(ts|tsx)?$': 'ts-jest',
+//   '^.+\\.(js|jsx)$': 'babel-jest',
+// };
+
+// jest.config.js
+export default {
+  transform: {
+    "^.+\\.tsx?$": "babel-jest"
+  },
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
--- api/swagger.js
+++ api/swagger.js
@@ -11,9 +11,7 @@ const doc = {
 const outputFile = './swagger-output.json';
 const routes = ['src/router/authentication.ts', 'src/router/index.ts', 'src/router/tasks.ts', 'src/router/users.ts'];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen()(outputFile, routes, doc).then(async () => {
-    await import('./src/index.ts'); // Your project's root file
+    await import('./src/index.ts');
   });
